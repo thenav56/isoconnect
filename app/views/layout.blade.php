242c6fa7230a4db56acbe5d5da2ef319e76b5258
thenav56 <navinayer56@gmail.com>
2015-02-03 22:15:31 +0545

    fixed scroll problem

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -27,7 +27,6 @@ nav {
 }
 
     </style>
-
  
 </head>
  
@@ -131,6 +130,22 @@ nav {
 
 {{HTML::script(asset('assests/js/jquery.min.js'))}}
 {{HTML::script(asset('assests/js/bootstrap.min.js'))}}
+  <script>
+          (function($){
+                window.onbeforeunload = function(e){    
+                  window.name += ' [' + $(window).scrollTop().toString() + '[' + $(window).scrollLeft().toString();
+                  };
+                $.maintainscroll = function() {
+                if(window.name.indexOf('[') > 0)
+                  {
+                  var parts = window.name.split('['); 
+                  window.name = $.trim(parts[0]);
+                  window.scrollTo(parseInt(parts[parts.length - 1]), parseInt(parts[parts.length - 2]));
+                  }   
+                };  
+                $.maintainscroll();
+                })(jQuery);
+    </script>
 @show
 </body>
 </html>
