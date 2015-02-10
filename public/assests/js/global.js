4acfcdee0df3d4d9c2c2d7f5805b3e100c3b961f
thenav56 <navinayer56@gmail.com>
2015-02-10 09:20:12 +0545

    re verification added and some fix

---
+++ public/assests/js/global.js
@@ -0,0 +1,26 @@
+
+
+
+    
+
+//auto suggest search bar
+    $(document).ready(function(){
+      var users = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        remote: <?php echo asset("'search/query?query=%QUERY'") ?>
+      });
+
+      users.initialize();
+
+      $('#users').typeahead({
+        hint: true,
+        highlight: true,
+        minLength: 2
+      }, {
+        name: 'users',
+        displayKey: 'name',
+        source: users.ttAdapter()
+       });
+    });
+
