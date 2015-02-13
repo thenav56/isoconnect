253713044d4d5b3bfec661faecb8e3b1db0c9d35
thenav56 <navinayer56@gmail.com>
2015-02-13 07:24:35 +0545

    add isoconnect about view

---
+++ app/views/isoconnect/about.blade.php
@@ -0,0 +1,26 @@
+
+@extends('layout')
+@section('head')
+<title>IsoConnect-Login</title>
+@stop
+
+@section('body')
+    <div class="col-lg-6   col-md-offset-3">
+    <div class="well bs-component">
+         
+                <fieldset>
+                    <h1>About US</h1>
+                    
+                     <ul>
+                     <li><h3> NavinIr </h3></li>
+                     <li><h3> PramodUn </h3></li>
+                     <li><h3> PPathak </h3></li>
+                     </ul>
+                     
+                </fieldset>
+             
+        </div>
+    </div>
+
+ 
+@stop
